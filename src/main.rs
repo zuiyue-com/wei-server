9e898a37b10b112a99e6c6968fa9858d9261a7c3
HuangWei <market@zuiyue.com>
2023-09-11 18:22:02 +0800

    Ensure there are no duplicate ports

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,59 @@
-use std::env;
 use axum::Router;
 use axum::routing::get;
 
 #[tokio::main]
-async fn main() {
-    wei_env::write(&wei_env::dir_bin(), "wei-server", &env::current_exe().unwrap().display().to_string()).unwrap();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    wei_env::bin_init("wei-server");
+    let instance = single_instance::SingleInstance::new("wei-server")?;
+    if !instance.is_single() { 
+        std::process::exit(1);
+    };
 
-    // 构建我们的路由表
-    let app = Router::new().route("/", get(|| async { "Hello, axum!" }));
+    let mut port = 1115;
 
-    // 运行应用程序
-    axum::Server::bind(&"0.0.0.0:1115".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    // 循环查找可用端口
+    while !is_port_available(port) {
+        port += 1;
+    }
+
+    // 把端口使用 serde_toml 写入数据到 .wei/server.dat
+    let file_server = wei_env::home_dir()? + "server.dat";
+    let mut server = std::fs::File::create(file_server)?;
+    let data = format!("{}", port);
+    use std::io::Write;
+    server.write_all(&data.into_bytes())?;
+
+    // axum 启动之后，不阻塞进程
+    let handle = tokio::spawn(async move {
+        // 构建我们的路由表
+        let app = Router::new().route("/", get(|| async { "Hello, axum!" }));
+
+        // 绑定port端口
+        let address = format!("127.0.0.1:{}", port);
+        println!("Server running on {}", address);
+        axum::Server::bind(&address.parse().unwrap())
+            .serve(app.into_make_service())
+            .await
+            .unwrap();
+    });
+
+    loop {
+        if wei_env::status() == "0" {
+            // 当程序接受到退出信号时，关闭 axum 服务
+            handle.abort();
+            return Ok(());
+        }
+
+        tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+    }
+
+}
+
+use std::net::TcpListener;
+
+fn is_port_available(port: u16) -> bool {
+    match TcpListener::bind(("127.0.0.1", port)) {
+        Ok(_) => true,
+        Err(_) => false,
+    }
 }
