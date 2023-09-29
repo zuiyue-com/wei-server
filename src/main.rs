0f3b127a226a5e49d7bf82aea3e6838d7e13030d
HuangWei <market@zuiyue.com>
2023-09-29 13:59:11 +0800

    add single instance

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ mod routes;
 
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    wei_env::bin_init("wei-server");
+    use single_instance::SingleInstance;
+    let instance = SingleInstance::new("wei-server").unwrap();
+    if !instance.is_single() { 
+        std::process::exit(1);
+    };
+
     let mut port = 1115;
 
     // 循环查找可用端口
