62aabf4b7372d867c452a5142d7398b5d92d6e28
HuangWei <market@zuiyue.com>
2023-10-09 09:09:17 +0800

    这个版本可以实现超时功能，但是没有办出内容。

--- src/routes/run.rs
+++ src/routes/run.rs
@@ -11,38 +11,61 @@ pub async fn index(Json(data): Json<Vec<String>>) -> String {
         return data.to_string();
     }
 
+    // use tokio::time::{timeout, Duration};
+
+    // let result = timeout(Duration::from_secs(5), run_task(command)).await.unwrap();
+
+    // match result {
+    //     Ok(value) => value,
+    //     Err(_) => {
+    //         let data = format!("{}", serde_json::json!({
+    //             "code": "400",
+    //             "msg": format!("任务超时")
+    //         }));
+    //         data
+    //     }
+    // }
+
+    use async_process::{Command, Child};
     use tokio::time::{timeout, Duration};
-
-    let handle = async {
-        match wei_run::run(command[0], command[1..].to_vec()) {
-            Ok(data) => data.to_string(),
-            Err(e) => {
-                let data = format!("{}", serde_json::json!({
-                    "code": "400",
-                    "msg": format!("{}", e)
-                }));
-                data
-            }
-        }
-    };
-
-    let result = timeout(Duration::from_secs(5), handle).await;
     
-    match result {
-        Ok(value) => value,
-        // Ok(Err(e)) => {
-        //     let data = format!("{}", serde_json::json!({
-        //         "code": "400",
-        //         "msg": format!("任务出错，错误：{}", e)
-        //     }));
-        //     data
-        // }
+    let mut child: Child = Command::new(command[0])
+        .args(command[1..].to_vec())
+        .spawn().unwrap();
+
+    let duration = Duration::from_secs(5);
+
+    match timeout(duration, child.status()).await {
+        Ok(result) => match result {
+            Ok(_) => {
+                let data = child.output().await.unwrap();
+                println!("here:{:?}", data);
+                let data = String::from_utf8_lossy(&data.stdout).into_owned();
+                format!("{}", 
+                serde_json::json!({
+                    "code": "200",
+                    "msg": "success",
+                    "data": data
+                }))
+            },
+            Err(e) => {
+                format!("{}", 
+                serde_json::json!({
+                    "code": "400",
+                    "msg": format!("任务执行失败: {}", e)
+                }))
+            }
+        },
         Err(_) => {
-            let data = format!("{}", serde_json::json!({
-                "code": "400",
-                "msg": format!("任务超时")
-            }));
-            data
+            // timeout elapsed, kill the process
+            child.kill().unwrap();
+            child.status().await.unwrap();
+
+            format!("{}", 
+                serde_json::json!({
+                    "code": "400",
+                    "msg": format!("任务超时")
+            }))
         }
     }
 }
