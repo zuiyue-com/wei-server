9082d6ff9682358d278e7975046b0285e8ec65a4
Wei <market@zuiyue.com>
2024-03-17 21:33:55 +0800

    功能: 更换single为wei-single

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ pub async fn main() -> Result<(), Box<dyn std::error::Error>> {
     wei_windows::init();
     wei_env::bin_init("wei-server");
     use single_instance::SingleInstance;
-    let instance = SingleInstance::new("wei-server").unwrap();
+    let instance = wei_single::SingleInstance::new("wei-server").unwrap();
     if !instance.is_single() { 
         std::process::exit(1);
     };
